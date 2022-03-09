a4e5c88244a230f672344f0d339de111cf7792c9
Cedric Le Sergent <lesergent.cedric@gmail.com>
2022-03-09 16:20:48 +0100

    Add Hashed Password #017

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -36,19 +37,15 @@ class RegisterType extends AbstractType
                     'placeholder' => 'Merci de saisir votre email'
                 ]
             ])
-            ->add('password', PasswordType::class, [
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Votre mot de passe et la confirmation doivent etre identique',
                 'label' => 'Votre mot de passe',
-                'attr' => [
-                    'placeholder' => 'Merci de saisir votre mot de passe'
-                ]
+                'required' => true,
+                'first_options' => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmez votre mot de passe'],
             ])
-            ->add('password_confirm', PasswordType::class, [
-                'label' => 'Votre mot de passe',
-                'mapped' => false,
-                'attr' => [
-                    'placeholder' => 'Merci de confirmer votre mot de passe'
-                ]
-            ])
+
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
             ])
